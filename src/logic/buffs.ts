48e5b17ccd548cefde5814acc33abf5b314b392e
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2022-04-27 23:07:41 +0200

    bonus psy et spectre

--- src/logic/buffs.ts
+++ src/logic/buffs.ts
@@ -1,6 +1,6 @@
 import { Alteration, AlterationType } from "../data/alterations"
 import { OWNER_PLAYER } from "../data/owners"
-import { TYPE_FEU } from "../data/types"
+import { TYPE_FEU, TYPE_PSY, TYPE_SPECTRE } from "../data/types"
 import { PokemonOnBoard } from "../objects/pokemon"
 import GameScene from "../scenes/GameScene"
 import { addAlteration } from "./alteration"
@@ -28,6 +28,25 @@ export function applyBuffs(pokemon: PokemonOnBoard){
                 const alteration: Alteration = { type: AlterationType.BRULURE, stacks: allianceState.stepReachedN+1 }
                 addAlteration(target, alteration, game)
                 console.log(`Buff FEU de ${pokemon.entry.name}: ${alteration.stacks} stacks de brûlure`)
+            }
+            pokemon.buffs.onHit.push(effect)
+        }
+
+        // BONUS ALLIANCE PSY
+        if(allianceState.type === TYPE_PSY && allianceState.stepReached){
+            const effect: OnHitEffect = ({ target }) => {
+                target.pp = Math.max(0, target.pp - allianceState.stepReachedN*2)
+                console.log(`Buff PSY de ${pokemon.entry.name}: la cible perd ${allianceState.stepReachedN*2}PP`)
+            }
+            pokemon.buffs.onHit.push(effect)
+        }
+
+        // BONUS ALLIANCE SPECTRE
+        if(allianceState.type === TYPE_SPECTRE && allianceState.stepReached){
+            const effect: OnHitEffect = ({ target }) => {
+                const alteration: Alteration = { type: AlterationType.PEUR, stacks: allianceState.stepReachedN*2 }
+                addAlteration(target, alteration, game)
+                console.log(`Buff PEUR de ${pokemon.entry.name}: ${alteration.stacks} stacks de peur`)
             }
             pokemon.buffs.onHit.push(effect)
         }
