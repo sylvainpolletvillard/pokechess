0ecf677c0c3774f9149eaede84cfb473bf780b40
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2022-06-24 23:31:48 +0200

    fix base path and remove pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa'
 import replace from '@rollup/plugin-replace';
 
 export default defineConfig({
+  base: "./",
   build: {
     rollupOptions: {
       plugins: [
